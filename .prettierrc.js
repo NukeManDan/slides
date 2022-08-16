4dddb241b32e692a6e6211d68d46c008d27dcda7
Dan Shields <nukemandan@protonmail.com>
2022-08-16 10:21:16 -0600

    init

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+};
